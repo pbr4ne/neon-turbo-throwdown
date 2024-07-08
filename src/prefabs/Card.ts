421e701e64223775e3d208b36ba0cda37a6c4e9d
pbr4ne <pbr4ne@protonmail.com>
2024-07-08 13:00:22 -0700

    feat: add type to card

--- src/prefabs/Card.ts
+++ src/prefabs/Card.ts
@@ -9,15 +9,24 @@ import Phaser from "phaser";
 
 export default class Card extends Phaser.GameObjects.Image {
 
+	public cardType: string;
+
 	constructor(scene: Phaser.Scene, x?: number, y?: number, texture?: string, frame?: number | string) {
 		super(scene, x ?? 310, y ?? 107, texture || "cardFront", frame);
 
-		/* START-USER-CTR-CODE */
-		// Write your code here.
-		/* END-USER-CTR-CODE */
-	}
+		this.cardType = ""; // Initialize with an empty string or a default type
+
+        this.setInteractive();
+    }
 
 	/* START-USER-CODE */
+
+    setType(type: string) {
+        this.cardType = type;
+        // You can add logic here to change the texture or frame based on the type
+    }
+
+	
 
 	// Write your code here.
 
