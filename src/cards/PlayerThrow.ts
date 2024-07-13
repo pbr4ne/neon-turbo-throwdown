987470368cb0828e5c28a7ca46cbc2ba44ec1604
pbr4ne <pbr4ne@protonmail.com>
2024-07-13 11:48:13 -0700

    feat: ultra turbo throw

--- src/cards/PlayerThrow.ts
+++ src/cards/PlayerThrow.ts
@@ -1,17 +1,24 @@
 import { Library } from "../throwdown/Library";
 import { TurboThrow } from "../trophies/TurboThrow";
+import { UltraTurboThrow } from "../trophies/UltraTurboThrow";
 import { Throw } from "./Throw";
 
 export class PlayerThrow extends Throw {
 
     getOffenseDamage(): number {
-        if (Library.getTrophyTypes().some(trophy => trophy instanceof TurboThrow)) {
+        if (Library.getTrophyTypes().some(trophy => trophy instanceof UltraTurboThrow)) {
+            return this.offenseDamage + 2;
+        }
+        else if (Library.getTrophyTypes().some(trophy => trophy instanceof TurboThrow)) {
             return this.offenseDamage + 1;
         }
         return this.offenseDamage;
     }
 
     getName(): string {
+        if (Library.getTrophyTypes().some(trophy => trophy instanceof UltraTurboThrow)) {
+            return "ultra turbo throw";
+        }
         if (Library.getTrophyTypes().some(trophy => trophy instanceof TurboThrow)) {
             return "turbo throw";
         }
@@ -19,6 +26,9 @@ export class PlayerThrow extends Throw {
     }
 
     getIcon(): string {
+        if (Library.getTrophyTypes().some(trophy => trophy instanceof UltraTurboThrow)) {
+            return "unknown";
+        }
         if (Library.getTrophyTypes().some(trophy => trophy instanceof TurboThrow)) {
             return "turbo-throw";
         }
