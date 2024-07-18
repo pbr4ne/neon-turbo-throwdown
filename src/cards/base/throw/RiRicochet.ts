019e41f5029c598062d4471ac62ccf0cab9ec28a
pbr4ne <pbr4ne@protonmail.com>
2024-07-18 10:42:49 -0700

    feat: nerfing ricochet

--- src/cards/base/throw/RiRicochet.ts
+++ src/cards/base/throw/RiRicochet.ts
@@ -2,7 +2,7 @@ import { CardKeys } from "../../CardKeys";
 import { UltraTurboThrow } from "./UltraTurboThrow";
 
 export class RiRicochet extends UltraTurboThrow {
-    protected numTargets: number = 3;
+    protected numTargets: number = 2;
 
     constructor(key: CardKeys = CardKeys.THROW_5, upgradeKey: CardKeys | null = CardKeys.THROW_6) {
         super(key, upgradeKey);
