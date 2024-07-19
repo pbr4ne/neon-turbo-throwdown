05c0bfb494eca286c937ad04cb965a7a88bb8280
pbr4ne <pbr4ne@protonmail.com>
2024-07-19 09:44:41 -0700

    fix: ricochet num targets

--- src/cards/base/throw/Ricochet.ts
+++ src/cards/base/throw/Ricochet.ts
@@ -2,7 +2,7 @@ import { CardKeys } from "../../CardKeys";
 import { TurboThrow } from "./TurboThrow";
 
 export class Ricochet extends TurboThrow {
-    protected numTargets: number = 1;
+    protected numTargets: number = 2;
 
     constructor(key: CardKeys = CardKeys.THROW_3, upgradeKey: CardKeys | null = CardKeys.THROW_4) {
         super(key, upgradeKey);
