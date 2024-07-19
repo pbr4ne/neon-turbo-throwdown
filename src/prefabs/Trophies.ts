97bc14413ba5d6c260651580573a3a72354b359f
pbr4ne <pbr4ne@protonmail.com>
2024-07-19 00:20:01 -0700

    feat: hacky way to show trophies in nice order

--- src/prefabs/Trophies.ts
+++ src/prefabs/Trophies.ts
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import { Library } from '../throwdown/Library';
 import { log } from '../utilities/GameUtils';
+import { TrophyKey } from '../trophies/TrophyKey';
 
 export default class Trophies extends Phaser.GameObjects.Container {
     constructor(scene: Phaser.Scene) {
@@ -24,7 +25,70 @@ export default class Trophies extends Phaser.GameObjects.Container {
         log("library trophy types");
         const trophies = Library.getTrophyTypes();
         log(trophies.toString());
-        const sortedTrophies = trophies.slice().sort((a, b) => a.getName().localeCompare(b.getName()));
+
+        //HACK
+        const trophyOrder = [
+            TrophyKey.CYAN_DECK,
+            TrophyKey.YELLOW_DECK,
+            TrophyKey.WHITE_DECK,
+            TrophyKey.BLACK_DECK,
+            TrophyKey.RED_DECK,
+
+            TrophyKey.CARD_INSIGHT_1,
+            TrophyKey.CARD_INSIGHT_2,
+            TrophyKey.CARD_INSIGHT_3,
+            TrophyKey.CARD_INSIGHT_4,
+
+            TrophyKey.TARGET_INSIGHT_1,
+            TrophyKey.TARGET_INSIGHT_2,
+            TrophyKey.TARGET_INSIGHT_3,
+            TrophyKey.TARGET_INSIGHT_4,
+
+            TrophyKey.HEALTH_INSIGHT_1,
+            TrophyKey.HEALTH_INSIGHT_2,
+            TrophyKey.HEALTH_INSIGHT_3,
+            TrophyKey.HEALTH_INSIGHT_4,
+
+            TrophyKey.INCREASE_HP_1,
+            TrophyKey.INCREASE_HP_2,
+
+            TrophyKey.HEALTH_REGEN_1,
+            TrophyKey.HEALTH_REGEN_2,
+            TrophyKey.HEALTH_REGEN_3,
+
+            TrophyKey.RESURRECT_1,
+
+            TrophyKey.IDLE_SPEED_1,
+            TrophyKey.IDLE_SPEED_2,
+            TrophyKey.IDLE_SPEED_3,
+            TrophyKey.IDLE_SPEED_4,
+            TrophyKey.OFFENSIVE_STRATEGY_1,
+            TrophyKey.DEFENSIVE_STRATEGY_1,
+            TrophyKey.HEAL_STRATEGY_1,
+
+            TrophyKey.THROW_2,
+            TrophyKey.THROW_3,
+            TrophyKey.THROW_4,
+            TrophyKey.THROW_5,
+            TrophyKey.THROW_6,
+
+            TrophyKey.BLOCK_2,
+            TrophyKey.BLOCK_3,
+            TrophyKey.BLOCK_4,
+            TrophyKey.BLOCK_5,
+
+            TrophyKey.EVADE_2,
+            TrophyKey.EVADE_3,
+            TrophyKey.EVADE_4,
+
+            TrophyKey.CATCH_2,
+            TrophyKey.CATCH_3,
+        ];
+
+        const sortedTrophies = trophies.slice().sort((a, b) => {
+            return trophyOrder.indexOf(a.getKey()) - trophyOrder.indexOf(b.getKey());
+        });
+
         log(sortedTrophies.toString());
 
         const maxPopupHeight = 700;
