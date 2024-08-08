ce0a6468ddc589ff2777d3226c352d6b91905afe
pbr4ne <pbr4ne@protonmail.com>
2024-08-08 15:43:40 -0700

    feat: shorten name of increase HP trophies

--- src/trophies/member/IncreaseHP1.ts
+++ src/trophies/member/IncreaseHP1.ts
@@ -3,6 +3,6 @@ import { TrophyType } from "../TrophyType";
 
 export class IncreaseHP1 extends TrophyType {
     constructor() {
-        super(TrophyKey.INCREASE_HP_1, "increase hp 1", "Increase team members' HP to 4.");
+        super(TrophyKey.INCREASE_HP_1, "add hp 1", "Increase team members' HP to 4.");
     }
 }
